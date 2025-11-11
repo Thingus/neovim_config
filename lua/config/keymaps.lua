9a5bdc1ee79d5de0445147afefb3daa4e7f0ac2f
Thingus <john.roberts@flowminder.org>
2025-11-11 11:05:37 +0000

    quick comment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -77,6 +77,7 @@ require("neo-tree").setup({
 	},
 })
 
+-- Telescope jumping around
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
