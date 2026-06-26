ddc643cfd3e61bb8d151b7ee54c000a2500245d9
Thingus <john.roberts@indeximate.com>
2026-06-26 13:39:01 +0100

    Adding workspace-diagnostics (but I don't thinkt it works)

--- lua/plugins/setup_by_hand.lua
+++ lua/plugins/setup_by_hand.lua
@@ -183,7 +183,11 @@ return {
 	{
 		"folke/trouble.nvim",
 		opts = {}, -- for default options, refer to the configuration section for custom setup.
-		cmd = "Trouble"
+		cmd = "Trouble",
+	},
+
+	{
+		"artemave/workspace-diagnostics.nvim",
 	},
 
 	-- rustaceanvim: Rust dev plugin
